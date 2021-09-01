8c9eb6c969908c94559d01662f2171671445759b
Mark <marwfair@gmail.com>
2021-09-01 13:25:33 -0500

    Removed android code.

--- lib/flutter_cue_light_show_sdk.dart
+++ lib/flutter_cue_light_show_sdk.dart
@@ -1,14 +1,7 @@
 import 'dart:async';
 
-import 'package:flutter/services.dart';
-
 class FlutterCueLightShowSdk {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_cue_light_show_sdk');
+  static Future<void> fetchTheme() async {}
 
-  static Future<void> fetchTheme() async {
-  }
-
-  static Future<void> launchCue() async {
-  }
+  static Future<void> launchCue() async {}
 }
