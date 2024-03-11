347f3e5262baf77b80be75da274c9e8790df1a22
Logan Bennett <bennett.loganj@gmail.com>
2024-03-11 12:18:21 -0500

    added prefetch function stub for the mock branch

--- lib/flutter_cue_light_show_sdk.dart
+++ lib/flutter_cue_light_show_sdk.dart
@@ -6,4 +6,6 @@ class FlutterCueLightShowSdk {
   static Future<void> launchCue() async {}
 
   static Future<void> launchCueV2(String url) async {}
+
+  static Future<void> prefetchCueV2(String urlString) async {}
 }
